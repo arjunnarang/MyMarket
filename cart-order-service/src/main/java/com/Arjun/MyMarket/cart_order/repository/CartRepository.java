6e9382a77f1c818c02a6d717d9efac27fb5a0f2f
arjunnarang <arjun.12narang@gmail.com>
2026-07-17 16:21:36 +0530

    some modifications

--- cart-order-service/src/main/java/com/Arjun/MyMarket/cart_order/repository/CartRepository.java
+++ cart-order-service/src/main/java/com/Arjun/MyMarket/cart_order/repository/CartRepository.java
@@ -1,7 +1,13 @@
 package com.Arjun.MyMarket.cart_order.repository;
 
+import com.Arjun.MyMarket.cart_order.entity.Cart;
 import com.Arjun.MyMarket.cart_order.entity.CartItem;
+import com.Arjun.MyMarket.cart_order.entity.CartStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<CartItem, Long> {
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUserIdAndStatus(String userId, CartStatus status);
 }
