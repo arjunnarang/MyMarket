f31f323b688c1841ddfd78c19c4551458d62d061
arjunnarang <arjun.12narang@gmail.com>
2026-07-25 14:15:55 +0530

    Api gateway service

---
+++ api-gateway/src/test/java/com/Arjun/MyMarket/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arjun.MyMarket.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
