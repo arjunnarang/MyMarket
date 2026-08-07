536c15bdd705985dffb97930bca1a8ef4c8dc190
arjunnarang <arjun.12narang@gmail.com>
2026-08-07 19:30:23 +0530

    making route for cart order service and applying retry on cart order service

--- api-gateway/src/main/java/com/Arjun/MyMarket/apigateway/RouteConfig.java
+++ api-gateway/src/main/java/com/Arjun/MyMarket/apigateway/RouteConfig.java
@@ -5,12 +5,18 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import java.time.Duration;
 
 @Configuration
 public class RouteConfig {
 
     @Value("${product.service.id}")
     private String productServiceId;
+
+    @Value("${cartorder.service.id}")
+    private String cartOrderServiceId;
     @Bean
     public RouteLocator route(RouteLocatorBuilder builder){
         return builder.routes()
@@ -26,6 +32,22 @@ public class RouteConfig {
                                                                         .setFallbackUri("forward:/product-fallback"))
                                         .rewritePath("/product-service/?(?<remaining>.*)", "/${remaining}"))
                                 .uri(productServiceId))
+
+                .route("cart-order-service", route ->
+                        route.path("/cart-order-service/**")
+                                .filters(f -> f
+                                        .addRequestHeader("x-api-gateway", "value from api gateway")
+                                        .rewritePath("/cart-order-service/?(?<remaining>.*)", "/${remaining}")
+                                        .retry(retryConfig -> retryConfig
+                                                .setRetries(3)
+                                                .setMethods(HttpMethod.GET, HttpMethod.POST)
+                                                .setBackoff(
+                                                        Duration.ofMillis(100),
+                                                        Duration.ofMillis(1000),
+                                                        2,
+                                                        true
+                                                )))
+                                .uri(cartOrderServiceId))
                 .build();
     }
 
